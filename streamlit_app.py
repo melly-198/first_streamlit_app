30a76139d345a098e5f5983320ac66e2238af371
melly-198 <137906579+melly-198@users.noreply.github.com>
2023-06-27 20:15:43 +0530

    Update streamlit_app.py
    missed ' added it

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Bluebery Oatmeal')
-streamlit.text('Kale, Spinach & Rocket smoothie)
+streamlit.text('Kale, Spinach & Rocket smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
