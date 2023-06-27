85a124b01a6cd6f7b14c65040c3c8e95663b3ca5
melly-198 <137906579+melly-198@users.noreply.github.com>
2023-06-27 20:17:00 +0530

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluebery Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
